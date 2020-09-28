d06899c41dcf9916bec284d221976bedc6de653e
inbarm1 <inbarmoskovich@gmail.com>
2020-09-28 12:44:50 +0300

    widget start

---
+++ FlashbackApp/app/src/main/java/com/flashbackapp/Utils.java
@@ -0,0 +1,4 @@
+package com.flashbackapp;
+
+public class Utils {
+}
---
+++ FlashbackApp/app/src/main/java/com/flashbackapp/WidgetProvider.java
@@ -0,0 +1,123 @@
+package com.flashbackapp;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RemoteViews;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
+import java.util.TimeZone;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static com.flashbackapp.MainActivity.GetStringArray;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    private static final String ACTION_CLICK = "ACTION_CLICK";
+    private DatabaseReference mDatabase;
+    GoogleAccountCredential credential;
+    private static final int REQUEST_AUTHORIZATION = 2;
+    private static final int CALL_SMS_PERMISSION_REQUESTS=3;
+    final JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+    FirebaseAuth firebaseAuth;
+    GoogleSignInClient googleSignInClient;
+
+    // Locations properties
+    protected LocationManager locationManager;
+
+    private static final String[] LOCATION_PERMISSIONS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+    };
+    private static final int INITIAL_REQUEST=1337;
+    private static final int METERS_THRESHOLD = 500;
+
+    private Location currentLocation;
+    private String closestLocation;
+    private String practiceApp;
+    private DatabaseReference locationsDatabase;
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        // Get all ids
+        ComponentName thisWidget = new ComponentName(context,
+                WidgetProvider.class);
+        int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+        for (int widgetId : allWidgetIds) {
+            // Create some random data
+            int number = (new Random().nextInt(100));
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.app_widget);
+            // Set the text
+//            remoteViews.setTextViewText(R.id.update, String.valueOf(number));
+
+            // Register an onClickListener
+            Intent intent = new Intent(context, WidgetProvider.class);
+
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//            remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+        }
+    }
+}
